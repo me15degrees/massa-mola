2397b757fd6a15d9d82a3662569acff917e53bf6
Maria Eduarda <me15degrees@gmail.com>
2024-04-09 21:16:10 -0300

    adicionando modificações de frame e data.clear

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 from vpython import *
 
-#objetos
-parede = box(pos=vector(-20,0,0), size = vector(1,10,10), color = color.yellow)
-bloco = box(pos=vector(5,-3,0), size = vector(4,4,4), color = color.red)
-solo = box(pos=vector(0,-5,0), size = vector(50,0.5,10), color = color.green)
-mola = helix(pos=vector(-20.5,-3,0), radius = 1.5, thickness = 0.5 , coils = 5, color = color.blue)
+def menu():
+    m = float(input("digite o valor da massa do bloco (kg): "))
+    k = float(input("digite o valor da constante elástica: "))
+    return m,k
 
+def main():
+    m, k = menu()
 
-#graficos
-g1 = graph(xtitle="tempo", ytitle='posicao')
-g2 = graph(xtitle="tempo", ytitle='velocidade')
+    #objetos
+    parede = box(pos=vector(-20,0,0), size = vector(1,10,10), color = color.yellow)
+    bloco = box(pos=vector(5,-3,0), size = vector(4,4,4), color = color.red)
+    solo = box(pos=vector(0,-5,0), size = vector(50,0.5,10), color = color.green)
+    mola = helix(pos=vector(-20.5,-3,0), radius = 1.5, thickness = 0.5 , coils = 5, color = color.blue)
 
-grafico1 = gcurve(graph = g1, color=color.red)
-grafico2 = gcurve(graph = g2, color=color.blue)
+    #condicoes iniciais
+    bloco.vel = vector(0,0,0)
+    t=0
+    dt=0.0005
 
-#vinculo
-mola.axis = bloco.pos - mola.pos
+    #graficos
+    g1 = graph(xtitle="tempo", ytitle='posicao')
+    g2 = graph(xtitle="tempo", ytitle='velocidade')
 
-#condicoes iniciais
-bloco.vel = vector(0,0,0)
-k=400
-m=5
-t=0
-dt=0.00001
+    grafico1 = gcurve(graph = g1, color=color.red)
+    grafico2 = gcurve(graph = g2, color=color.blue)
 
-
-#equacoes
-while True:
-    rate(1/dt)
-    f = vector(-bloco.pos.x * k, 0, 0)
-    acel = f/m            
-    bloco.pos = bloco.pos + (bloco.vel * dt)
-    bloco.vel = bloco.vel + (acel*dt)
-       
+    #vinculo
     mola.axis = bloco.pos - mola.pos
-    t = t + dt
-    
-    grafico1.plot(t, bloco.pos.x)
-    grafico2.plot(t, bloco.vel.x)
-    
+
+    #equacoes
+    while True:
+        rate(1/dt)
+        f = vector(-bloco.pos.x * k, 0, 0)
+        acel = f/m            
+        bloco.pos = bloco.pos + (bloco.vel * dt)
+        bloco.vel = bloco.vel + (acel*dt)
+        
+        mola.axis = bloco.pos - mola.pos
+        t = t + dt
+        
+        grafico1.plot(t, bloco.pos.x)
+        grafico2.plot(t, bloco.vel.x)
+
+        grafico1.data.clear()
+        grafico1.data.clear()
+
+if __name__ == "__main__":
+    main()
